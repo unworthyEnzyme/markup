d36b1d1a74ee2c25d9bfebaab5679edeaa2d9cc8
unworthyEnzyme <unworthy_enzyme@protonmail.com>
2023-01-25 18:21:01 +0300

    i can finally parse my syntax to json

--- src/ast.rs
+++ src/ast.rs
@@ -1,23 +1,25 @@
-#[derive(Debug, Clone, PartialEq, Eq)]
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
 pub struct Tag<'source> {
     pub name: &'source str,
     pub attributes: Vec<Attribute<'source>>,
     pub children: Vec<Node<'source>>,
 }
 
-#[derive(Debug, Clone, PartialEq, Eq)]
+#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
 pub enum Node<'source> {
     Tag(Tag<'source>),
     String(&'source str),
 }
 
-#[derive(Debug, Clone, PartialEq, Eq)]
+#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
 pub struct Attribute<'source> {
     pub(crate) name: &'source str,
     pub(crate) value: Literal<'source>,
 }
 
-#[derive(Debug, Clone, PartialEq, Eq)]
+#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
 pub enum Literal<'source> {
     Number(u32),
     String(&'source str),
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use std::fs;
+
+use markup::parser::Parser;
+
 fn main() {
-    println!("Hello, world!");
+    let source = fs::read_to_string("source.txt").unwrap();
+    let node = Parser::new().parse(source.as_bytes()).unwrap();
+    let serialized = serde_json::to_string_pretty(&node).unwrap();
+    fs::write("source.json", &serialized).unwrap();
 }
